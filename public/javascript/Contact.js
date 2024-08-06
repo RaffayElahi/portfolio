ad9a451c34eba9bd2f91b68eff3ac713f554f731
Raffay Elahi <honeybee33392@gmail.com>
2024-08-07 03:41:08 +0500

    Fixed flickering issue on /contact route

--- public/javascript/Contact.js
+++ public/javascript/Contact.js
@@ -1,161 +1,171 @@
-window.addEventListener('resize', ()=> {
-    location.reload();
-  
-  });
-  window.addEventListener('beforeunload', () => {
-    window.scrollTo(0, 0);
-  });
-  
-  window.addEventListener('load', () => {
-    window.scrollTo(0, 0);
-  
-  });
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    options = {
-      "outerStyle": "circle",
-      "hoverEffect": "circle-move",
-      "hoverItemMove": false,
-      "defaultCursor": false,
-      "outerWidth": 30,
-      "outerHeight": 30
-    }
-    magicMouse(options);
-  
-      const tlMobileToggle = gsap.timeline({paused:true, reversed:true})
-      const mobileToggle = document.querySelector('.mobile-toggle')
-      tlMobileToggle.fromTo('.line:nth-child(2)',{
+window.addEventListener('load', () => {
+  window.scrollTo(0, 0);
+
+  document.fonts.ready.then(() => {
+    const images = Array.from(document.images);
+    const imagesLoaded = images.map(img => {
+      return new Promise((resolve, reject) => {
+        if (img.complete) {
+          resolve();
+        } else {
+          img.onload = resolve;
+          img.onerror = reject;
+        }
+      });
+    });
+
+    Promise.all(imagesLoaded).then(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      // GSAP Timeline for Loader
+      const tl = gsap.timeline();
+      const loader = document.querySelector(".site-loader");
+      const inner = document.querySelector(".loader-img");
+      tl.from(inner, {
+        y: 200,
+        delay: 1,
+        duration: 1,
+        ease: "power3.inOut",
+      });
+      tl.to(loader, {
+        y: "-100%",
+        delay: 0.5,
+        ease: "linear",
+        duration: 1,
+      });
+
+      // Magic Mouse Initialization
+      const options = {
+        outerStyle: "circle",
+        hoverEffect: "circle-move",
+        hoverItemMove: false,
+        defaultCursor: false,
+        outerWidth: 30,
+        outerHeight: 30,
+      };
+      magicMouse(options);
+
+      // GSAP Timeline for Mobile Toggle
+      const tlMobileToggle = gsap.timeline({ paused: true, reversed: true });
+      const mobileToggle = document.querySelector('.mobile-toggle');
+      tlMobileToggle.fromTo('.line:nth-child(2)', {
         width: '80%',
         position: 'static',
-        rotate:0
-      },{
-        width:'0',
+        rotate: 0
+      }, {
+        width: '0',
         ease: 'power1.inOut',
         duration: 0.2,
-        
       }, 'line')
-      tlMobileToggle.fromTo('.line:first-child',{
-        position:'static',
+      .fromTo('.line:first-child', {
+        position: 'static',
         width: '40%',
-        rotate:0
-      },{
-        width:'100%',
+        rotate: 0
+      }, {
+        width: '100%',
         position: 'absolute',
-        rotate: '135 deg',
+        rotate: '135deg',
         ease: 'power1.inOut',
-        duration: 0.2
+        duration: 0.2,
       }, 'line')
-      tlMobileToggle.fromTo('.line:last-child',{
+      .fromTo('.line:last-child', {
         position: 'static',
         width: '50%',
-        rotate:0
-      },{
-        position:'absolute',
-        width:'100%',
-        rotate: '45 deg',
+        rotate: 0
+      }, {
+        position: 'absolute',
+        width: '100%',
+        rotate: '45deg',
         ease: 'power1.inOut',
-        duration: 0.2
+        duration: 0.2,
       }, 'line')
-      tlMobileToggle.fromTo('.mobile-nav-cover', {
+      .fromTo('.mobile-nav-cover', {
         height: 0,
-        display:'none'
-      },{
-        minHeight:'100%',
+        display: 'none'
+      }, {
+        minHeight: '100%',
         ease: 'power1.inOut',
         duration: 1,
-        display:'flex'
+        display: 'flex'
       }, 'display')
-      tlMobileToggle.fromTo('.mobile-nav-links-upper',{
+      .fromTo('.mobile-nav-links-upper', {
         display: "none",
-      },{
+      }, {
         display: "flex",
       }, 'display')
-      tlMobileToggle.fromTo('.mobile-nav-links-upper a',{
+      .fromTo('.mobile-nav-links-upper a', {
         display: "none",
-      },{
+      }, {
         display: "block"
       }, 'display')
-      tlMobileToggle.fromTo('.mobile-nav-links-lower',{
+      .fromTo('.mobile-nav-links-lower', {
         display: "none"
-      },{
+      }, {
         display: "flex"
       }, 'display')
-      tlMobileToggle.fromTo('.partial-important-mobile-nav',{
+      .fromTo('.partial-important-mobile-nav', {
         display: "none"
-      },{
+      }, {
         display: "flex"
       }, 'display')
-      tlMobileToggle.fromTo('.website-policies-mobile-nav',{
+      .fromTo('.website-policies-mobile-nav', {
         display: "none"
-      },{
+      }, {
         display: "block"
       }, 'display')
-  
-  
-      tlMobileToggle.fromTo('.nav-text-mobile',{
-        y:100
-      },{
-        y:0,
-        duration:0.3,
-        stagger:0.2,
-        yoyo:true
-  
+      .fromTo('.nav-text-mobile', {
+        y: 100
+      }, {
+        y: 0,
+        duration: 0.3,
+        stagger: 0.2,
+        yoyo: true
       })
-      tlMobileToggle.fromTo('.nav-text-mobile-anim',{
-        y:100
-      },{
-        y:0,
-        duration:0.3,
-        stagger:0.2,
-        yoyo:true
-  
-      })
-      mobileToggle.addEventListener('click', () => {
-        if(tlMobileToggle.reversed()){
-          tlMobileToggle.play()
-        }else{
-          tlMobileToggle.reverse()
-        }
-      })
-      
-  
-    })
-    const hero = document.querySelector(".selected h1")
-    gsap.to(hero, {
-      scrollTrigger: {
-        trigger: document.body,
-        scrub: 2,
-      }
-    })
-  
-  const sect = document.querySelectorAll('.scroll-item')
-  sect.forEach((sectt) => {
-    gsap.to(sectt, {
-      transform: 'translateX(-100%)',
-      repeat: -1,
-      duration: 10,
-      ease: 'linear'
-    })
-  })
-  const allHead = document.querySelectorAll('.scroll-item h2')
-  allHead.forEach((h) => {
-    gsap.from(h, {
-      y: -500,
-      duration: 1,
-      delay: 0.1,
-      scrollTrigger: {
-        trigger: h,
-      }
-    })
-  })
+      .fromTo('.nav-text-mobile-anim', {
+        y: 100
+      }, {
+        y: 0,
+        duration: 0.3,
+        stagger: 0.2,
+        yoyo: true
+      });
 
-  const yearChangingText = document.querySelector('.rights p');
-  const date = new Date()
-  const currentYear = date.getFullYear();
-  yearChangingText.innerHTML = `Raffay Elahi ${currentYear} | All rights reserved &#169.`
-  gsap.registerPlugin(ScrollTrigger);
-  
-  
-  
-  
-  
+      mobileToggle.addEventListener('click', () => {
+        if (tlMobileToggle.reversed()) {
+          tlMobileToggle.play();
+        } else {
+          tlMobileToggle.reverse();
+        }
+      });
+
+
+      const sect = document.querySelectorAll('.scroll-item');
+      sect.forEach((sectt) => {
+        gsap.to(sectt, {
+          transform: 'translateX(-100%)',
+          repeat: -1,
+          duration: 10,
+          ease: 'linear'
+        });
+      });
+
+      const allHead = document.querySelectorAll('.scroll-item h2');
+      allHead.forEach((h) => {
+        gsap.from(h, {
+          y: -500,
+          duration: 1,
+          delay: 0.1,
+          scrollTrigger: {
+            trigger: h,
+          }
+        });
+      });
+
+      // Update Year Text
+      const yearChangingText = document.querySelector('.rights p');
+      const date = new Date();
+      const currentYear = date.getFullYear();
+      yearChangingText.innerHTML = `Raffay Elahi ${currentYear} | All rights reserved &#169.`;
+    });
+  });
+});
