d11efd95d759379365be78293c3f2df0dcfd8de1
Raffay Elahi <honeybee33392@gmail.com>
2024-08-07 03:11:24 +0500

    Optimized assests

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -22,7 +22,25 @@ window.addEventListener("load", () => {
     Promise.all(imagesLoaded).then(() => {
 
       gsap.registerPlugin(ScrollTrigger);
+      const tl = gsap.timeline()
+      const loader = document.querySelector(".site-loader")
+      const inner = document.querySelector(".loader-img")
+      tl.from(inner,{
+          y: 200,
+          delay:1,
+          duration: 1,
+          ease: "power3.inOut",
 
+          
+      })
+      tl.to(loader, {
+          y: "-100%",
+          delay: 0.5,
+          ease: "power3.inOut",
+          duration:1,
+          ease: "linear",
+          
+      })
 
       const options = {
         outerStyle: "circle",
